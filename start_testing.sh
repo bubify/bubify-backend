0d07c91d0601e6e3d2772836c6fb5fe626da6ff0
erma7725 <erik.magnehed.7725@student.uu.se>
2024-03-26 13:37:25 +0100

    Test module starts and output to txt-file

--- start_testing.sh
+++ start_testing.sh
@@ -9,12 +9,5 @@ export TERM=xterm
 
 rm -rf target
 
-while true; do
-  # TODO: Change the profile to test, should run the tests and then terminate
-  mvn spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=development -Dspring-boot.run.jvmArguments=-XX:+UseG1GC -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
-
-  while true; do 
-    watch -d -t -g "ls -lR src | sha1sum" && mvn compile && curl http://localhost:8900/internal/restart
-  done
-
-done
+# Start tests and stores a copy of results to a file
+mvn test -Dspring.profiles.active=application-test -Dmaven.test.failure.ignore=true | tee /home/bubify/backend/test_results.txt
